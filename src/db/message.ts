2639e1faaecedd24ffa58fdec4ab859194b74bf3
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-04-04 21:42:55 -0500

    Add message, user, and index files

---
+++ src/db/message.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema } from "mongoose";
+
+const MessageSchema = new mongoose.Schema({
+    to: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    sender: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    subject: {
+        type: String,
+        required: true,
+    },
+    body:{
+        type: String,
+
+    },
+    socket:{
+        type: String,
+    },
+    status:{
+        type: Number,
+        default: 1,
+    }
+ 
+},{
+    timestamps: true,
+});
+
+export const MessageModel = mongoose.model("Message", MessageSchema);
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import { createServer } from 'http';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+import { colors } from './utils/constants/colors';
+import { connectToMongoDB } from './db/config/dbconfig';
+
+
+import routes from './routes';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use('/',routes());
+
+const server = createServer(app);
+
+connectToMongoDB();
+
+server.listen(PORT, () => {
+  console.log(colors.magenta,`Server running on port ${PORT}`)
+  console.log(colors.reset);
+
+});
