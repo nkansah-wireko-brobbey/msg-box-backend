1c6ed4185854c9e19a6c1b482a9d07867fc95db1
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-04-13 21:38:54 -0500

    Add message, user to index routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,13 +2,18 @@ import express from 'express';
 
 import authenticationRoutes from './Authentication';
 import profileRoutes from './Profile'
+import messageRoutes from './Message'
+import userRoutes from './User'
 
 const router = express.Router();
 
 export default (): express.Router => {
 
-   authenticationRoutes(router);
+    authenticationRoutes(router);
     profileRoutes(router);
+    messageRoutes(router)
+    userRoutes(router)
+
 
     return router;
 }
