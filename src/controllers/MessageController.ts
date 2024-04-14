e1e28f7b0212bdce908f2e3af438d023269790c1
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-04-13 21:38:23 -0500

    Refactor MessageController methods: getAllmessage, readMessage, and deleteMessage

--- src/controllers/MessageController.ts
+++ src/controllers/MessageController.ts
@@ -4,7 +4,7 @@ import { MessageSchema } from "../utils/validations";
 
 class MessageController{
 
-    public async getAllmessage(request: Request, response: Response){
+    public async getAllMessage(request: Request, response: Response){
 
         try{
             const messages = await MessageModel.find();
@@ -58,6 +58,53 @@ class MessageController{
         }
          
 
+    }
+
+    public async readMessage(request: Request, response: Response){
+        try{
+            const {id} = request.params;
+
+            const message = await MessageModel.findById(id)
+
+            if(!message){
+                throw Error("No data found")
+            }
+
+            message.status = 2;
+
+            await message.save();
+
+            return response.status(200)
+            .json({message:  'Message updated to read', data: message})
+
+        }catch(error){
+
+            return response.status(400).json({status: false, error})
+
+        }
+    }
+    public async deleteMessage(request: Request, response: Response){
+        try{
+            const {id} = request.params;
+
+            const message = await MessageModel.findById(id)
+
+            if(!message){
+                throw Error("No data found")
+            }
+
+            message.status = 3;
+
+            await message.save();
+
+            return response.status(200)
+            .json({message:  'Message deleted', data: message})
+
+        }catch(error){
+
+            return response.status(400).json({status: false, error})
+
+        }
     }
     
 }
